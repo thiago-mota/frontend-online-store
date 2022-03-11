c2996d37d2cb226ab6f87535eb0e7005be8a1323
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-03-11 11:46:33 -0300

    Requisito 5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      products: [],
+    };
+  }
+
+  handleInput = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  fetchProducts = async () => {
+    const { name } = this.state;
+    const response = await getProductsFromCategoryAndQuery('', name);
+    const data = response.results;
+    console.log(data);
+
+    this.setState({
+      name: '',
+      products: data,
+    });
+  }
+
   render() {
+    const { name, products } = this.state;
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <input
+          type="text"
+          name="name"
+          data-testid="query-input"
+          value={ name }
+          onChange={ this.handleInput }
+          placeholder="Digite o nome do produto"
+        />
+        <button
+          type="submit"
+          data-testid="query-button"
+          onClick={ this.fetchProducts }
+        >
+          Pesquisar
+        </button>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        { products === 0
+          ? <p>Nenhum produto foi encontrado</p>
+          : (
+            products.map((product) => (
+              <div
+                key={ product.id }
+                data-testid="product"
+              >
+                <p>{product.id}</p>
+                <p>{ product.title}</p>
+                <p>{product.price}</p>
+                <img src={ product.thumbnail } alt={ product.title } />
+              </div>
+            ))
+          )}
+      </div>
     );
   }
 }
